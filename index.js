8c076eafb59356d070cc8767996d1cbb63d8bf8e
akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
2022-11-02 20:45:19 +0900

    赤石JSファイル編集

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function display() {
     let pokeWeight;
     let pokeExp;
     let pokePic;
+    let pokeSpeUrl;
 
     const pokeNo = pokemonNames.indexOf(pokeName) + 1;
 
@@ -30,22 +31,31 @@ async function display() {
             pokeWeight = json.weight;
             pokeHight = json.height
             pokePic = json.sprites.other["official-artwork"]["front_default"];
+            pokeSpeUrl =  json.species.url;
         })
     console.log("promise", pokeHight, pokeWeight, pokePic);
 
+    await fetch(pokeSpeUrl).then(res => res.json())
+        .then(json => {
+            pokeType = json.genera.find(lag => lag.language.name === "ja").genus;
+            pokeExp = json.flavor_text_entries.filter(obj => obj.language.name === "ja" && obj.version.name === "x")[0].flavor_text
+        })
+    console.log(pokeExp[0]);
     // const imageEl = document.getElementById("pokePic");
     const imageEl = document.createElement("img");
     imageEl.src = pokePic;
     imageEl.width = 300
     document.getElementById("pokePic").append(imageEl);
 
+    document.getElementById("pokeType").innerText += "   " + pokeType;
+    document.getElementById("pokeExp").innerText += "   \n" + pokeExp;
     document.getElementById("pokeName").innerText += "  " + pokeName;
     document.getElementById("pokeHight").innerText += "  " + pokeHight + " m";
-    document.getElementById("pokeWeight").innerText += "  " + pokeWeight + "Kg";
+    document.getElementById("pokeWeight").innerText += "  " + pokeWeight + " Kg";
 }
 
-function findUnderWeight(weight) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`).then(res => res.json())
-        .then(json => json.results.map(obj => fetch(obj.url).then(res => res.json())))
-        .then(arr => Promise.all(arr).then(res => res.filter(elm => Number(elm.weight) < weight)));
-}
+// function findUnderWeight(weight) {
+//     return fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`).then(res => res.json())
+//         .then(json => json.results.map(obj => fetch(obj.url).then(res => res.json())))
+//         .then(arr => Promise.all(arr).then(res => res.filter(elm => Number(elm.weight) < weight)));
+// }
