d545f58b56cba63a5f49b020df213cb770db99ff
akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
2022-11-02 21:17:47 +0900

    赤石JSファイル編集2テスト追加

---
+++ test/_index.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+escribe("pokemon zukan", () => {
+    it("should be function display()", () => {
+        expect(typeof display).to.equal("function");
+    })
+})
