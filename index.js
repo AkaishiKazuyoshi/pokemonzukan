0c2a0e63893da6bac152c59e544883af74a333bb
akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
2022-11-02 23:31:37 +0900

    エクストラ作成赤石2

--- index.js
+++ index.js
@@ -3,26 +3,29 @@ console.log("hello");
 
 const pokemonNames = ["フシギダネ", "フシギソウ", "フシギバナ", "ヒトカゲ", "リザード", "リザードン", "ゼニガメ", "カメール", "カメックス"];
 const ohtaniA = ["大谷翔平", "大谷翔平", "大谷翔平", "大谷翔平", "大谷翔平", "大谷翔平", "大谷翔平", "大谷翔平"];
-const digMenber = ["あびるまん", "えりこさん", "こたろーさん", "kikiさん", "みずきさん", "ありささん", "つばささん", "ゆーすけさん", "みくさん", "かけるさん", "りこさん"];
+const digMenber = ["あびるまん", "たまろーさん", "えりこさん", "こたろーさん", "kikiさん", "みずきさん", "ありささん", "つばささん", "ゆーすけさん", "みくさん", "かけるさん", "りこさん"];
 
 const extraObj = {
-    大谷翔平: {},
-    あびるまん: {},
-    えりこさん: {},
-    こたろーさん: {},
-    kikiさん:{},
-    みづきさん:{},
-    ありささん:{},
-    つばささん: {},
-    ゆーすけさん:{},
-    みくさん:{},
-    かけるさん:{},
-    りこさん:{},
-    
+    大谷翔平: { img: "images/ohtani.jpeg", name: "大谷翔平", type: "ああ", hight: "いい", weight: "うう", explain: "大" },
+    あびるまん: { img: "images/abiru.png", name: "あびるまん", type: "", hight: "？？", weight: "？？", explain: "" },
+    たまろーさん: { img: "", name: "たまろーさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    えりこさん: { img: "", name: "えりこさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    こたろーさん: { img: "", name: "こたろーさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    kikiさん: { img: "", name: "kikiさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    みづきさん: { img: "", name: "みづきさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    ありささん: { img: "", name: "ありささん", type: "", hight: "？？", weight: "？？", explain: "" },
+    つばささん: { img: "", name: "つばささん", type: "", hight: "？？", weight: "？？", explain: "" },
+    ゆーすけさん: { img: "", name: "ゆーすけさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    みくさん: { img: "", name: "みくさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    かけるさん: { img: "", name: "かけるさん", type: "", hight: "？？", weight: "？？", explain: "" },
+    りこさん: { img: "", name: "りこさん", type: "", hight: "？？", weight: "？？", explain: "" },
 }
 
 function pokeSelectAp() {
     const pokeListEl = document.getElementById("pokeSelect");
+    while (pokeListEl.lastChild) {
+        pokeListEl.removeChild(pokeListEl.lastChild)
+    }
     pokemonNames.forEach(element => {
         let opttionEl = document.createElement("option");
         opttionEl.value = element;
@@ -33,6 +36,9 @@ function pokeSelectAp() {
 
 function ohtani() {
     const pokeListEl = document.getElementById("pokeSelect");
+    while (pokeListEl.lastChild) {
+        pokeListEl.removeChild(pokeListEl.lastChild)
+    }
     ohtaniA.forEach(element => {
         let opttionEl = document.createElement("option");
         opttionEl.value = element;
@@ -43,6 +49,9 @@ function ohtani() {
 
 function dig() {
     const pokeListEl = document.getElementById("pokeSelect");
+    while (pokeListEl.lastChild) {
+        pokeListEl.removeChild(pokeListEl.lastChild)
+    }
     digMenber.forEach(element => {
         let opttionEl = document.createElement("option");
         opttionEl.value = element;
@@ -65,23 +74,32 @@ async function display() {
     let pokeExp;
     let pokePic;
     let pokeSpeUrl;
+    console.log(digMenber.indexOf(pokeName) !== -1, "aaaa", pokeName === "大谷翔平");
+    console.log(extraObj[pokeName]);
+    if (digMenber.indexOf(pokeName) !== -1 || pokeName === "大谷翔平") {
+        pokePic = extraObj[pokeName].img;
+        pokeType = extraObj[pokeName].type;
+        pokeHight = extraObj[pokeName].height;
+        pokeWeight = extraObj[pokeName].weight;
+        pokeExp = extraObj[pokeName].explain;
+    } else {
+        const pokeNo = pokemonNames.indexOf(pokeName) + 1;
 
-    const pokeNo = pokemonNames.indexOf(pokeName) + 1;
+        await fetch(`https://pokeapi.co/api/v2/pokemon/` + pokeNo).then(res => res.json())
+            .then(json => {
+                pokeWeight = json.weight;
+                pokeHight = json.height
+                pokePic = json.sprites.other["official-artwork"]["front_default"];
+                pokeSpeUrl = json.species.url;
+            })
+        console.log("promise", pokeHight, pokeWeight, pokePic);
 
-    await fetch(`https://pokeapi.co/api/v2/pokemon/` + pokeNo).then(res => res.json())
-        .then(json => {
-            pokeWeight = json.weight;
-            pokeHight = json.height
-            pokePic = json.sprites.other["official-artwork"]["front_default"];
-            pokeSpeUrl = json.species.url;
-        })
-    console.log("promise", pokeHight, pokeWeight, pokePic);
-
-    await fetch(pokeSpeUrl).then(res => res.json())
-        .then(json => {
-            pokeType = json.genera.find(lag => lag.language.name === "ja").genus;
-            pokeExp = json.flavor_text_entries.filter(obj => obj.language.name === "ja" && obj.version.name === "x")[0].flavor_text
-        })
+        await fetch(pokeSpeUrl).then(res => res.json())
+            .then(json => {
+                pokeType = json.genera.find(lag => lag.language.name === "ja").genus;
+                pokeExp = json.flavor_text_entries.filter(obj => obj.language.name === "ja" && obj.version.name === "x")[0].flavor_text
+            })
+    }
     console.log(pokeExp[0]);
     // const imageEl = document.getElementById("pokePic");
     // const imageEl = document.createElement("img");
